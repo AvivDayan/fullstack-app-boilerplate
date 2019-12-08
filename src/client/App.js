473942722ca32de79ba78340ee3f38dfc28c3727
Aviv Dayan <avivdayan@gmail.com>
2019-12-09 01:56:42 +0200

    Inital commit

--- src/client/App.js
+++ src/client/App.js
@@ -1,23 +1,42 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import './app.css';
 import ReactImage from './react.png';
 
-export default class App extends Component {
-  state = { username: null };
+// export default class App extends Component {
+//   state = { username: null };
 
-  componentDidMount() {
+//   componentDidMount() {
+//     fetch('/api/getUsername')
+//       .then(res => res.json())
+//       .then(user => this.setState({ username: user.username }));
+//   }
+
+//   render() {
+//     const { username } = this.state;
+//     return (
+//       <div>
+//         {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
+//         <img src={ReactImage} alt="react" />
+//       </div>
+//     );
+//   }
+// }
+
+const App = () => {
+  const [userName, setUserName] = useState();
+
+  useEffect(() => {
     fetch('/api/getUsername')
       .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
-  }
+      .then(user => setUserName(user.username));
+  }, [])
 
-  render() {
-    const { username } = this.state;
-    return (
-      <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
-      </div>
-    );
-  }
+  return (
+    <div>
+      {userName ? <h1>{`Hello ${userName}`}</h1> : <h1>Loading.. please wait!</h1>}
+      <img src={ReactImage} alt="react" />
+    </div>
+  )
 }
+
+export default App;
